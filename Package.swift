49b576c7dfd328717f53e894e9ce1e8219a79353
Anton Dityativ <adityativ@ronasit.com>
2022-04-11 12:11:34 +0600

    updated configuration

--- Package.swift
+++ Package.swift
@@ -5,21 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSugar",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v11),
+        .watchOS(.v4)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "UIKit+Sugar", targets: ["UIKit+Sugar"]),
         .library(name: "QuartzCore+Sugar", targets: ["QuartzCore+Sugar"]),
         .library(name: "Foundation+Sugar", targets: ["Foundation+Sugar"]),
         .library(name: "CoreGraphics+Sugar", targets: ["CoreGraphics+Sugar"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/malcommac/SwiftDate.git", .upToNextMajor(from: "6.3.1")),
-        .package(url: "https://github.com/RonasIT/swift-networking.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/malcommac/SwiftDate.git", .upToNextMajor(from: "6.3.1"))
     ],
     targets: [
         .target(name: "UIKit+Sugar", path: "Sources/UIKit+Sugar"),
         .target(name: "QuartzCore+Sugar", path: "Sources/QuartzCore+Sugar"),
-        .target(name: "Foundation+Sugar", path: "Sources/Foundation+Sugar"),
+        .target(
+            name: "Foundation+Sugar",
+            dependencies: ["SwiftDate"],
+            path: "Sources/Foundation+Sugar"
+        ),
         .target(name: "CoreGraphics+Sugar", path: "Sources/CoreGraphics+Sugar"),
     ]
 )
